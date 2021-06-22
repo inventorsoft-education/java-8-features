ac68373875a29ab61f2540e131616d39d85e3d11
Viktor Fylypchuk <vikfil87@gmail.com>
2021-06-22 19:10:31 +0300

    deleted unnecessary import

--- src/main/java/co/inventorsoft/StreamHomework.java
+++ src/main/java/co/inventorsoft/StreamHomework.java
@@ -2,8 +2,6 @@ package co.inventorsoft;
 
 import co.inventorsoft.model.Person;
 import co.inventorsoft.model.User;
-
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
